22e2c846fb6a6b8e09370fd9c8958e3d351483bf
DmitroKurskiy <kurskiy315920@gmail.com>
2021-10-24 16:01:00 +0300

    comment

--- fifthLesson/firstTask2.php
+++ fifthLesson/firstTask2.php
@@ -1,24 +1,26 @@
 <?php
-$arr = ['small string','biggest of the string', 'average string',];
 
-$content = maxstr($arr);
-
+$arr = ['small string','biggest of the string', 'average string'];
 function maxstr($a)
 {
     $max = [];
     foreach ($a as $key => $item) {
-        $max[] .= strlen($item);
-        $c = max($max);
+        $max[] .= strlen(trim($item));
+        $maxLenght = max($max);
     }
-
+    $maxString = '';
     foreach ($a as $item) {
-        if (strlen($item) == $c){
-            echo $item . "<br>";
+        if (strlen(trim($item)) != $maxLenght){
+            continue;
+        }
+        else {
+            $maxString .= $item . PHP_EOL;
         }
     }
+    return $maxString;
 }
+echo maxstr($arr);
 
-echo $content;
 
 
 
